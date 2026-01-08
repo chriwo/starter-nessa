406f0e47273020b7ed04ff1a50a6a4aab076a456
Christian Wolfram <c.wolfram@chriwo.de>
2026-01-08 09:30:26 +0100

    [TASK] Setup Code QA to run with DDEV
    To execute code quality tools use `ddev qa`. This command execute PHP-CS-Fixer, Rector, Fractor and PHPStan.

--- fractor.php
+++ fractor.php
@@ -6,17 +6,18 @@ use a9f\Fractor\Configuration\FractorConfiguration;
 use a9f\Typo3Fractor\Set\Typo3LevelSetList;
 
 return FractorConfiguration::configure()
-    ->withPaths([__DIR__ . '/'])
+    ->withPaths([
+        __DIR__ . '/',
+    ])
     ->withSets([
         Typo3LevelSetList::UP_TO_TYPO3_13,
     ])
     ->withSkip([
         // Skip paths
-        getcwd() . '/.idea',
-        getcwd() . '/.Build',
-        getcwd() . '/.ddev',
-        getcwd() . '/.project',
-        getcwd() . '/config',
-        getcwd() . '/var',
-        getcwd() . '/Resources/Private/frontendSrc',
+        __DIR__ . '/.idea',
+        __DIR__ . '/.Build',
+        __DIR__ . '/.ddev',
+        __DIR__ . '/config',
+        __DIR__ . '/var',
+        __DIR__ . '/Resources/Private/frontendSrc',
     ]);
--- rector.php
+++ rector.php
@@ -4,30 +4,25 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\ValueObject\PhpVersion;
 use Ssch\TYPO3Rector\Set\Typo3LevelSetList;
 use Ssch\TYPO3Rector\Set\Typo3SetList;
 
 return RectorConfig::configure()
     ->withPaths([
-        getcwd() . '/',
+        __DIR__ . '/',
     ])
     ->withImportNames(false, true, false, true)
-    ->withPhpVersion(PhpVersion::PHP_83)
     ->withSets([
         Typo3SetList::CODE_QUALITY,
         Typo3SetList::GENERAL,
         Typo3LevelSetList::UP_TO_TYPO3_13,
-        LevelSetList::UP_TO_PHP_83,
         PHPUnitSetList::ANNOTATIONS_TO_ATTRIBUTES,
     ])
     ->withSkip([
         // Skip paths
-        getcwd() . '/.Build',
-        getcwd() . '/.ddev',
-        getcwd() . '/.project',
-        getcwd() . '/config',
-        getcwd() . '/var',
-        getcwd() . '/Resources/Private/frontendSrc',
+        __DIR__ . '/.Build',
+        __DIR__ . '/.ddev',
+        __DIR__ . '/config',
+        __DIR__ . '/var',
+        __DIR__ . '/Resources/Private/frontendSrc',
     ]);
