230e7e531b3450b70759f3bfbebb1a31f71d16d5
Christian Wolfram <c.wolfram@chriwo.de>
2026-03-02 11:34:10 +0100

    [TASK] Add teaser variant with mouse hover effect

--- Configuration/TCA/Overrides/tt_content_teaser.php
+++ Configuration/TCA/Overrides/tt_content_teaser.php
@@ -34,6 +34,11 @@ defined('TYPO3') || die();
                             'icon' => 'starter-table-tx_starternessa_teaser_element',
                         ],
                         [
+                            'label' => $translationFile . 'tt_content.nessa_teaser_type.I.image_text_hover',
+                            'value' => 'image_text_hover',
+                            'icon' => 'starter-table-tx_starternessa_teaser_element',
+                        ],
+                        [
                             'label' => $translationFile . 'tt_content.nessa_teaser_type.I.div_icon',
                             'value' => '--div--',
                         ],
